e7f013ff8ea3b68dc77355a4fb4331e27b9bdef1
luis.poveda93 <luis.poveda9321@gmail.com>
2022-03-07 22:50:19 +0100

    constructor was changed

--- contracts/Books.sol
+++ contracts/Books.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Bookverse is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract Bookverse is ERC721, ERC721Burnable, ERC721Enumerable, Ownable {
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    uint public constant MAX_SUPPLY = 100;
-    uint public constant PRICE = 0.01 ether;
-    uint public constant MAX_PER_MINT = 5;
+    uint public constant MAX_SUPPLY = 20;
+    uint public constant PRICE = 0.001 ether;
+    uint public constant MAX_PER_MINT = 3;
     
     string public baseTokenURI;
 
@@ -28,14 +28,14 @@ contract Bookverse is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     function reserveNFTs() public onlyOwner {
         uint totalMinted = _tokenIdCounter.current();
         require(
-            totalMinted.add(10) < MAX_SUPPLY, "Not enough NFTs"
+            totalMinted.add(5) < MAX_SUPPLY, "Not enough NFTs"
         );
-        for (uint i = 0; i < 10; i++) {
+        for (uint i = 0; i < 5; i++) {
             _mintSingleNFT();
         }
     }
 
-    function _baseURI() internal pure override returns (string memory) {
+    function _baseURI() internal view virtual override returns (string memory) {
         return baseTokenURI;
     }
 
@@ -78,25 +78,27 @@ contract Bookverse is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         require(success, "Transfer failed.");
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
+   function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function tokenURI(uint256 tokenId)
+    function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
     {
-        return super.tokenURI(tokenId);
+        return super.supportsInterface(interfaceId);
     }
 }
