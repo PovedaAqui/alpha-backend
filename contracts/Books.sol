55d8a688c10f540d75acd7adc2b949fca86ddd64
luis.poveda93 <luis.poveda9321@gmail.com>
2022-03-07 22:35:03 +0100

    functions added

--- contracts/Books.sol
+++ contracts/Books.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -12,10 +14,68 @@ contract Bookverse is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Bookverse", "BOOKS") {}
+    uint public constant MAX_SUPPLY = 100;
+    uint public constant PRICE = 0.01 ether;
+    uint public constant MAX_PER_MINT = 5;
+    
+    string public baseTokenURI;
+
+
+    constructor(string memory baseURI) ERC721("Bookverse", "BOOKS") {
+        setBaseURI(baseURI);
+    }
+
+    function reserveNFTs() public onlyOwner {
+        uint totalMinted = _tokenIdCounter.current();
+        require(
+            totalMinted.add(10) < MAX_SUPPLY, "Not enough NFTs"
+        );
+        for (uint i = 0; i < 10; i++) {
+            _mintSingleNFT();
+        }
+    }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://";
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    function mintNFTs(uint _count) public payable {
+        uint totalMinted = _tokenIdCounter.current();
+
+        require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs left!");
+        require(_count >0 && _count <= MAX_PER_MINT, "Cannot mint specified number of NFTs.");
+        require(msg.value >= PRICE.mul(_count), "Not enough ether to purchase NFTs.");
+
+        for (uint i = 0; i < _count; i++) {
+            _mintSingleNFT();
+        }
+    }
+    
+    function _mintSingleNFT() private {
+        uint newTokenID = _tokenIdCounter.current();
+        _safeMint(msg.sender, newTokenID);
+        _tokenIdCounter.increment();
+    }
+
+    function tokensOfOwner(address _owner) external view returns (uint[] memory) {
+        uint tokenCount = balanceOf(_owner);
+        uint[] memory tokensId = new uint256[](tokenCount);
+
+        for (uint i = 0; i < tokenCount; i++) {
+            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokensId;
+    }
+    
+    function withdraw() public payable onlyOwner {
+        uint balance = address(this).balance;
+        require(balance > 0, "No ether left to withdraw");
+        (bool success, ) = (msg.sender).call{value: balance}("");
+        require(success, "Transfer failed.");
     }
 
     function safeMint(address to, string memory uri) public onlyOwner {
